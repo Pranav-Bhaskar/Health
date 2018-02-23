6e5b65a3d31985415a25262e6ed1bab0d2b48763
Rohit096 <35897843+Rohit096@users.noreply.github.com>
2018-02-23 18:28:53 +0530

    nice health

--- app/src/main/java/com/kaneki/health/MainActivity.java
+++ app/src/main/java/com/kaneki/health/MainActivity.java
@@ -3,9 +3,13 @@ package com.kaneki.health;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button b1;
@@ -15,23 +19,33 @@ public class MainActivity extends AppCompatActivity {
     private EditText weight;
     double bmi;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // diabetic button
+        // diabetic button
 
         b1= (Button)findViewById(R.id.button3);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                E=(EditText)findViewById(R.id.health_et_3);
-              double value = Double.parseDouble( E.getText().toString());
-              if (value < 7)
-                  setContentView(R.layout.fitdiabetes);
-              else
-                setContentView(R.layout.diabetes);
+                E = (EditText) findViewById(R.id.health_et_3);
+                if (TextUtils.isEmpty(E.getText().toString()))
+                {
+                    Toast.makeText(getApplicationContext(), "Enter Sugar Level", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    double value = Double.parseDouble(E.getText().toString());
+                    if (value < 100)
+                        setContentView(R.layout.fitdiabetes);
+                    else {
+                        setContentView(R.layout.diabetes);
+                        TextView textView = (TextView) findViewById(R.id.health_et_3);
+                        textView.setMovementMethod(new ScrollingMovementMethod());
+
+                    }
+
+                }
             }
         });
 
@@ -45,22 +59,41 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 height=(EditText)findViewById(R.id.health_et_1);
                 weight=(EditText)findViewById(R.id.health_et_2);
-
-                double v = Double.parseDouble( height.getText().toString());
-
-                double z = Double.parseDouble( weight.getText().toString());
-                if(v>=0&&z>=0) {
-
-                    bmi = z / v;
-                    if (bmi >= 25)
-                        setContentView(R.layout.overweight);
-                    else if (bmi <= 19)
-                        setContentView(R.layout.underweight);
-                    else if (bmi > 19 && bmi < 25)
-                        setContentView(R.layout.fitbmi);
-
+                if (TextUtils.isEmpty(height.getText().toString())){
+                    Toast.makeText(getApplicationContext(), "Enter Height", Toast.LENGTH_LONG).show();
+                }
+                else if(TextUtils.isEmpty(weight.getText().toString())){
+                    Toast.makeText(getApplicationContext(), "Enter Weight", Toast.LENGTH_LONG).show();
                 }
 
+                else{
+                    double v = Double.parseDouble( height.getText().toString());
+
+                    double z = Double.parseDouble( weight.getText().toString());
+                    if(v>=0&&z>=0) {
+
+                        bmi = z / (v*v) ;
+                        if (bmi >= 25) {
+                            setContentView(R.layout.overweight);
+                            TextView ow_tv_1 =(TextView) findViewById(R.id.ow_tv_1) ;
+                            ow_tv_1.setText("BMI: "+z);
+                            TextView textView = (TextView) findViewById(R.id.ow_tv_2);
+                            textView.setMovementMethod(new ScrollingMovementMethod());
+                        }
+
+                        else if (bmi <= 19){
+                            setContentView(R.layout.underweight);
+                            TextView uw_tv_1 =(TextView) findViewById(R.id.uw_tv_1) ;
+                            uw_tv_1.setText("BMI: "+z);
+                            TextView textView = (TextView) findViewById(R.id.uw_tv_2);
+                            textView.setMovementMethod(new ScrollingMovementMethod());
+
+                        }
+                        else
+                            setContentView(R.layout.fitbmi);
+
+                    }
+                }
 
             }
         });
